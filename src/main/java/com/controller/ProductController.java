c34ab1a8d11d33a8d37de992fb387333cbb9bdaa
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-03-31 21:30:54 +0200

    Added more resources #7

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,31 +1,32 @@
 package com.controller;
 
-import com.model.product.ProductService;
-import com.model.product.ProductServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class ProductController {
 
-    Logger logger = LoggerFactory.getLogger(ProductController.class);
-
-    private static ProductService productService;
-
-    private static ProductService getProductService() {
-        if (productService != null) {
-            return productService;
-        }
-        productService = new ProductServices();
-        return productService;
-    }
     @GetMapping("/products")
-    public List startDefault() {
-        logger.trace("Call");
-        return getProductService().getProducts();
+    public List<String> getProducts() {
+        List<String> products = new ArrayList<>();
+        products.add("Product");
+        products.add("Product");
+        products.add("Product");
+        return products;
+    }
+
+    @GetMapping("/products/{Id}")
+    public String getProductWithId(@PathVariable int Id) {
+        return String.format("Product: %s", Id);
+    }
+
+    @GetMapping("/products/{Id}/categories")
+    public String getCategoriesWithProductId(@PathVariable int Id) {
+        return String.format("Category: %s", Id);
     }
 }
