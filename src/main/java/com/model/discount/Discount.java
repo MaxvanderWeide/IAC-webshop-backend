8ee4f48349657f5ad591f2a6119f8f1acf3fc29c
Nick Klomp <nick.klomp@student.hu.nl>
2020-04-05 00:49:51 +0200

    Updated Discount Class #15

--- src/main/java/com/model/discount/Discount.java
+++ src/main/java/com/model/discount/Discount.java
@@ -6,14 +6,27 @@ import java.util.Date;
 import java.util.List;
 
 public class Discount {
+    private int id;
     private Date from;
     private Date until;
-    private List<Product> products;
+    private Double price;
+    private int product;
+    private String description;
+//    private List<Product> products;
 
-    public Discount(Date from, Date until, List<Product> products) {
+//    public Discount(Date from, Date until, List<Product> products) {
+//        this.from = from;
+//        this.until = until;
+//        this.products = products;
+//    }
+
+    public Discount(int id, Date from, Date until, Double price, int product, String descript){
+        this.id = id;
         this.from = from;
         this.until = until;
-        this.products = products;
+        this.price = price;
+        this.product = product;
+        this.description = descript;
     }
 
     //Getters and Setters
@@ -24,4 +37,28 @@ public class Discount {
     //Until
     public Date getUntil() {return until;}
     public void setUntil(Date until) {this.until = until;}
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getProduct() {
+        return product;
+    }
+
+    public void setProduct(int product) {
+        this.product = product;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/persistence/discount/DiscountDAOImpl.java
+++ src/main/java/com/persistence/discount/DiscountDAOImpl.java
@@ -1,6 +1,45 @@
 package com.persistence.discount;
 
+import com.model.discount.Discount;
+import com.model.product.Product;
 import com.persistence.BaseDAO;
+import com.service.ConfigSelector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DiscountDAOImpl extends BaseDAO implements DiscountDAO {
+
+//    @Override
+    public Discount getDiscountWithId(int id) {
+        ResultSet rs;
+        Discount discount = null;
+        String query = String.format("SELECT * FROM `%s`.discount WHERE discountID = ?;", ConfigSelector.SCHEMA);
+
+        try (Connection conn = getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+
+            rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                discount = new Discount(
+                        rs.getInt(1),
+                        rs.getDate(2),
+                        rs.getDate(3),
+                        rs.getDouble(4),
+                        rs.getInt(5),
+                        rs.getString(6)
+                );
+            }
+
+            return discount;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
