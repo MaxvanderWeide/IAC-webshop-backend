279e45fcd9f779c55dc37e3e050963647eefd20d
milo <milo.voorhout@student.hu.nl>
2020-04-04 11:18:09 +0200

    Newly created product has as category 'nieuw' and if that category does not yet exist it is created #21

--- src/main/java/com/persitence/ProductDAOImpl.java
+++ src/main/java/com/persitence/ProductDAOImpl.java
@@ -3,10 +3,7 @@ package com.persitence;
 import com.model.product.Product;
 import com.service.ConfigSelector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ProductDAOImpl extends BaseDAO implements ProductDAO {
 
@@ -17,17 +14,17 @@ public class ProductDAOImpl extends BaseDAO implements ProductDAO {
         String query = String.format("SELECT * FROM `%s`.product WHERE productID = ?;", ConfigSelector.SCHEMA);
 
         try (Connection conn = getConnection();
-                PreparedStatement preparedStatement = conn.prepareStatement(query)){
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
             preparedStatement.setInt(1, id);
 
             rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 product = new Product(
-                    rs.getInt(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getDouble(4),
-                    rs.getInt(5)
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getInt(5)
                 );
             }
 
@@ -40,14 +37,19 @@ public class ProductDAOImpl extends BaseDAO implements ProductDAO {
 
     @Override
     public boolean saveProduct(Product product) {
-        String query = String.format("INSERT INTO `%s`.product (name, description, price, categoryID) VALUE (?, ?, ?, ?);", ConfigSelector.SCHEMA);
+        String createQuery = String.format("INSERT INTO `%s`.product (name, description, price, categoryID) VALUE (?, ?, ?, ?);", ConfigSelector.SCHEMA);
+        int categoryID = getNewCategoryID();
+
+        if (categoryID == 0) {
+            return false;
+        }
 
         try (Connection conn = getConnection();
-                PreparedStatement preparedStatement = conn.prepareStatement(query)){
+             PreparedStatement preparedStatement = conn.prepareStatement(createQuery)) {
             preparedStatement.setString(1, product.getName());
             preparedStatement.setString(2, product.getDescription());
             preparedStatement.setDouble(3, product.getPrice());
-            preparedStatement.setInt(4, product.getCategoryID());
+            preparedStatement.setInt(4, categoryID);
 
             preparedStatement.execute();
 
@@ -56,5 +58,46 @@ public class ProductDAOImpl extends BaseDAO implements ProductDAO {
             e.printStackTrace();
         }
         return false;
+    }
+
+    //Gets the ID from category named 'nieuw'
+    public int getNewCategoryID() {
+        ResultSet rs;
+        String getQuery = String.format("SELECT categoryID FROM `%s`.category WHERE name = 'nieuw'", ConfigSelector.SCHEMA);
+
+        try (Connection conn = getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(getQuery)) {
+            rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+
+            return createCategoryNew();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    public int createCategoryNew() {
+        String createQuery = String.format("INSERT INTO `%s`.category (name, description) VALUE ('nieuw', 'categorie van nieuwe aangemaakte producten');", ConfigSelector.SCHEMA);
+
+        try (Connection conn = getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(createQuery, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.execute();
+
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
     }
 }
