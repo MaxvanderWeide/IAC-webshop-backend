535469a6f41a86e811179edc5c2504e5b0f342af
Nick Klomp <nick.klomp@student.hu.nl>
2020-04-05 17:18:35 +0200

    ProductController UnitTest #19

--- src/main/java/com/controller/ProductControllerTest.java
+++ src/main/java/com/controller/ProductControllerTest.java
@@ -1,23 +1,43 @@
 package com.controller;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.*;
 
-import java.util.Arrays;
-import java.util.List;
+import com.model.product.Product;
+import com.persistence.product.ProductDAO;
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ProductControllerTest {
 
+    @Autowired
+    private ProductDAO productDao;
+
     @Test
-    public void testgetProducts() throws Throwable{
-        // Given
-        ProductController test = new ProductController();
+    public void testFindById()
+    {
+        Product product = productDao.getProductWithId(5);
 
-        // When
-//        List<String> result = test.getProducts();
+        Assert.assertEquals(5, product.getId());
+        Assert.assertEquals("TestProduct", product.getName());
+        Assert.assertEquals("Mooi test product", product.getDescription());
+        Assert.assertEquals(30.00, product.getPrice());
+    }
 
-        List<String> expected = Arrays.asList("Product", "Product", "Product");
-        // THEN
-//        assertEquals(expected, result);
+    @Test
+    public void testSaveProduct()
+    {
+        Product product = new Product();
+        product.setName("TestSaveProduct");
+        product.setDescription("Dit is een Test");
+        product.setPrice(10.00);
+
+        productDao.saveProduct(product);
+        int id = product.getId();
+
+        Product newProduct = productDao.getProductWithId(id);
+
+        Assert.assertEquals("TestSaveProduct", newProduct.getName());
+        Assert.assertEquals("Dit is een Test", newProduct.getDescription());
+        Assert.assertEquals(10.00, newProduct.getPrice());
     }
 }
--- src/main/java/com/model/product/Product.java
+++ src/main/java/com/model/product/Product.java
@@ -44,8 +44,16 @@ public class Product {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public List<Category> getCategories() {
