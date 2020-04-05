18ee9a4b4df489a522f18930fcff8beee9cd983a
Nick Klomp <nick.klomp@student.hu.nl>
2020-04-05 23:31:46 +0200

    Updated Test Branch #19

--- src/test/java/com/iacweb/iacweb/IacwebApplicationTests.java
+++ src/test/java/com/iacweb/iacweb/IacwebApplicationTests.java
@@ -4,38 +4,35 @@ import com.iacweb.iacweb.controller.CategoryControllerTest;
 import com.iacweb.iacweb.controller.CustomerControllerTest;
 import com.iacweb.iacweb.controller.DiscountControllerTest;
 import com.iacweb.iacweb.controller.ProductControllerTest;
+import org.junit.internal.TextListener;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.notification.Failure;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.runner.Result;
 
 @SpringBootTest
 class IacwebApplicationTests {
 
-	@Test
-	void contextLoads() {
-        Result categoryResult = JUnitCore.runClasses(CategoryControllerTest.class);
-        Result customerResult = JUnitCore.runClasses(CustomerControllerTest.class);
-        Result discountResult = JUnitCore.runClasses(DiscountControllerTest.class);
-        Result productResult = JUnitCore.runClasses(ProductControllerTest.class);
+    @Test
+    void contextLoads() {
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
 
-        for (Failure failure : categoryResult.getFailures()) {
-            System.out.println(failure.toString());
-        }
-        for (Failure failure : customerResult.getFailures()) {
-            System.out.println(failure.toString());
-        }
-        for (Failure failure : discountResult.getFailures()) {
-            System.out.println(failure.toString());
-        }
-        for (Failure failure : productResult.getFailures()) {
-            System.out.println(failure.toString());
-        }
-            System.out.println(categoryResult.wasSuccessful());
-            System.out.println(customerResult.wasSuccessful());
-            System.out.println(discountResult.wasSuccessful());
-            System.out.println(productResult.wasSuccessful());
-        }
-	}
+        Result result = junit.run(
+                CategoryControllerTest.class,
+                CustomerControllerTest.class,
+                DiscountControllerTest.class,
+                ProductControllerTest.class);
+
+        resultReport(result);
+    }
+
+    public static void resultReport(Result result) {
+        System.out.println("Finished. Result: Failures: " +
+                result.getFailureCount() + ". Ignored: " +
+                result.getIgnoreCount() + ". Tests run: " +
+                result.getRunCount() + ". Time: " +
+                result.getRunTime() + "ms.");
+    }
+}
  
